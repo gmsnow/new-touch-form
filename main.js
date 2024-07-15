ea8377d63e12de7162205c1686251b2536bb5ccd
Hitham <snows6005@gmail.com>
2024-07-15 18:51:45 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+'use strict'
+const date = new Date();
+const options = {
+    weekday: 'long',
+}
+dateDate.value = `${date.toLocaleDateString('ar-EG', options)}/${date.getFullYear()}/${date.getMonth()}/${date.getDate()}`
+step.addEventListener('click',()=> {
+stepShow.classList.remove('hide');
+})
+mission.addEventListener('click',()=> {
+missionShow.classList.remove('hide');
+})
+gouls.addEventListener('click',()=> {
+    goulsShow.classList.remove('hide');
+    cardHider1.classList.add('col-xl-3');
+})
+sign.addEventListener('click',()=> {
+    signShow.classList.remove('hide');
+    cardHider1.classList.add('col-lg-3');
+})
+feild.addEventListener('click',()=> {
+    feildShow.classList.remove('hide');
+})
+knowledge.addEventListener('click',()=> {
+    knowledgeShow.classList.remove('hide'); 
+})
+term.addEventListener('click',()=> {
+    termShow.classList.remove('hide');
+})
+before.addEventListener('click',()=> {
+    beforeShow.classList.remove('hide');
+})
+during.addEventListener('click',()=> {
+    duringShow.classList.remove('hide');
+})
+after.addEventListener('click',()=> {
+    afterShow.classList.remove('hide');
+})
+prepare.addEventListener('click',()=> {
+    prepareShow.classList.remove('hide');
+})
+signature.addEventListener('click',()=> {
+    signatureShow.classList.remove('hide');
+})
+level.addEventListener('click',()=> {
+    levelShow.classList.remove('hide');
+})
+means.addEventListener('click',()=> {
+    meansShow.classList.remove('hide');
+})
+type.addEventListener('click',()=> {
+    typeShow.classList.remove('hide');
+})
+evaluation.addEventListener('click',()=> {
+    evaluationShow.classList.remove('hide');
+})
---
+++ variables.js
@@ -0,0 +1,42 @@
+const dateDate = document.querySelector('#data-date');
+const step = document.querySelector('.step')
+const cardHider1 = document.querySelector('.card-hider1')
+const transform = document.querySelector('.transform')
+const mission = document.querySelector('.mission');
+const card = document.querySelector('.card-hider');
+const stepShow = document.querySelector('.step-Show');
+const missionShow = document.querySelector('.mission-show');
+const logo = document.querySelector('.logo');
+const headerBtn = document.querySelector('.header-btn');
+const gouls = document.querySelector('.gouls');
+const goulsShow = document.querySelector('.gouls-show');
+const sign = document.querySelector('.sign');
+const signShow = document.querySelector('.sign-show');
+const feild = document.querySelector('.feild');
+const feildShow = document.querySelector('.feild-show');
+const knowledge = document.querySelector('.knowledge');
+const knowledgeShow = document.querySelector('.knowledge-show');
+const term = document.querySelector('.term');
+const termShow = document.querySelector('.term-show');
+const before = document.querySelector('.before');
+const beforeShow = document.querySelector('.before-show');
+const during = document.querySelector('.during');
+const duringShow = document.querySelector('.during-show');
+const after = document.querySelector('.after');
+const afterShow = document.querySelector('.after-show');
+const prepare = document.querySelector('.prepare');
+const prepareShow = document.querySelector('.prepare-show');
+const signature = document.querySelector('.signature');
+const signatureShow = document.querySelector('.signature-show');
+const level = document.querySelector('.level');
+const levelShow = document.querySelector('.level-show');
+const means = document.querySelector('.means');
+const meansShow = document.querySelector('.means-show');
+const type = document.querySelector('.type');
+const typeShow = document.querySelector('.type-show');
+const evaluation = document.querySelector('.evaluation');
+const evaluationShow = document.querySelector('.evaluation-show');
+
+
+
+
